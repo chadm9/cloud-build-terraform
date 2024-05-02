33f3b14362c7e25753cc4690a5ccd26d708fcde6
chadm9 <chadm_@yahoo.com>
2024-05-02 09:03:40 -0400

    build vpc

--- environments/dev/vpc.tf
+++ environments/dev/vpc.tf
@@ -1,4 +1,4 @@
-resource "google_compute_network" "default" {
+resource "google_compute_network" "cld_bld_vpc" {
   project                 = var.project
   name                    = "${local.env}-cld-bld-vpc"
   provider                = google-beta
